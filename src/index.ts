e960b4db6253fc94c2f31ded34f459026382293b
José Pinheiro <jose.pinheiro@cajuinasaogeraldo.com.br>
2025-12-09 08:19:10 -0300

    chore: upgrade Node.js runtime, update AWS region, increase timeout, enable CORS, adjust handler path, and remove dynamic GCP credential setup.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { app } from './app';
-import * as fs from 'fs';
 import serverless from 'serverless-http';
 
-// Setup GCP credentials from environment
-const credentials = process.env.GOOGLE_CREDENTIALS_JSON;
-if (credentials) {
-	fs.writeFileSync('/tmp/gcp-credentials.json', credentials);
-	process.env.GOOGLE_APPLICATION_CREDENTIALS = '/tmp/gcp-credentials.json';
-}
-
-export default serverless(app);
+export const handler = serverless(app);
